1d1b328c377b6d66961b9a96d178376ba184981e
Allmacdev <allmacdev@gmail.com>
2024-04-07 12:54:25 -0300

    Initial commit

---
+++ assets/js/github.js
@@ -0,0 +1,32 @@
+const repositories = document.querySelector('.github')
+
+function getApiGitHub(){
+    fetch('https://api.github.com/users/AllMaciente/repos')
+        .then(async res =>{
+            if(!res.ok){
+                throw new Error(res.status);
+            }
+
+            let data = await res.json();
+            data.map( item =>   {
+                let project = document.createElement('div');
+
+                project.innerHTML = `
+                <div class="project">
+                    <div>
+                        <h4 class="title">${ item.name}</h4>
+                        <span class="dateCreate">${ Intl.DateTimeFormat('pt-BR').format(new Date(item.created_at))}</span>
+                    </div>
+                    <div>
+                        <a href="${ item.html_url}" target="_blank">${ item.html_url}</a>
+                        <span class="language"><span class="circle"></span>${ item.language}</span>
+                    </div>
+                </div>
+                `
+
+                repositories.appendChild(project);
+            })
+        })
+}
+
+getApiGitHub()
